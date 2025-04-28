7f4562a887c432aa4d30418fca296a850fcb00ea
ankit <ankitraj9062@gmail.com>
2025-04-28 07:33:19 +0530

    commit2

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,8 @@ function Home() {
   return (
     <div>
         <h1> hello world </h1>
+
+         <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. A perspiciatis error laudantium mollitia, maxime cupiditate! Ea enim dolorem est iure!</p>
     </div>
   )
 }
